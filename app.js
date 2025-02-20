60ab0e30197e802b1b32216d428429983bf3de5a
Davi Fontes <dfcc.work@gmail.com>
2025-02-20 16:27:26 -0300

    modificado .html para condizer com as funções do javascript (addAmigo e SortAmigo), corrigido typo na linha 6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let amigos =[];
 function addAmigos() {
     let amigos = document.getElementById(amigo);
 
-    }
+
 }
 //updateAmigos irá inserir os nomes dados ao addAmigos no array amigos.
 function updateAmigos(){
